70d485695cc4f9ec057677853eae360029dc8acc
Antony Mwangi <antonymwangiw85@gmail.com>
2026-06-20 01:25:15 +0300

    category model

---
+++ models/Category.ts
@@ -0,0 +1,39 @@
+import mongoose, { Schema, Document, Model } from "mongoose";
+
+export interface ICategory extends Document {
+  name: string;
+  slug: string;
+  description?: string;
+}
+
+const CategorySchema = new Schema<ICategory>(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+
+    description: {
+      type: String,
+      default: "",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Category: Model<ICategory> =
+  mongoose.models.Category ||
+  mongoose.model<ICategory>("Category", CategorySchema);
+
+export default Category;
